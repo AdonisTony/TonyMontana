65e5c96e60d483923f39c55a51e58dfb3314bba4
AdonisTony <121732631+AdonisTony@users.noreply.github.com>
2023-02-23 22:12:35 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -38,7 +38,7 @@ function MyApp({ Component, pageProps }) {
   return (<>
     <Head>
         <title>Umut Bayraktar</title>
-        <link rel="shortcut icon" href="https://media.discordapp.net/attachments/737348411568685066/954506783777493072/Umut_Bayraktar_Icon.png" type="image/x-icon" ></link>
+        <link rel="shortcut icon" href="https://tenor.com/view/pain-gif-20260098" type="image/x-icon" ></link>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" />
 
