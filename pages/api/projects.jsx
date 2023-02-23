88435bccba74505b863faf41b852128fd1116b55
AdonisTony <121732631+AdonisTony@users.noreply.github.com>
2023-02-23 22:00:34 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,7 +2,7 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://media.discordapp.net/attachments/737348411568685066/954502826791428136/codeshare_logo.png",
+      image: "https://pixelbox.ru/wp-content/uploads/2021/09/anime-gif-discord-16.gif",
       name: "Code Share",
       description: "Discord türkiyenin en büyük discord bot komutları arşivi ve en büyük discord botu uptime, hostlayıcı websitesi. Code Share veritabanında discord botlarına özgü yüzlerce discord bot komutu bulunmaktadır ve kendi discord botunuzu yaratmanıza yardımcı olur.",
       link: "https://codeshare.me",
