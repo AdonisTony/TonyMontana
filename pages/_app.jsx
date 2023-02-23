06506eb82c0942a9c0fd8779750564b1df17e2ee
AdonisTony <121732631+AdonisTony@users.noreply.github.com>
2023-02-23 22:21:42 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,7 +37,7 @@ function MyApp({ Component, pageProps }) {
     });
   return (<>
     <Head>
-        <title>Umut Bayraktar</title>
+        <title>Tony Montana</title>
         <link rel="shortcut icon" href="https://tenor.com/view/pain-gif-20260098" type="image/x-icon" ></link>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" />
