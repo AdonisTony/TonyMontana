48ee0c7a750fcb2f5ab3d0f8c818499186400325
AdonisTony <121732631+AdonisTony@users.noreply.github.com>
2023-02-23 23:04:51 +0300

    Update projects.jsx

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -10,15 +10,15 @@ export default function Home() {
   return (
     <>
       <div className="py-20">
-        <p className="text-3xl text-white font-semibold text-center">My Projects</p>
-        <p className="text-xl text-white/50 font-normal text-center mb-5">The projects that I am actively working on are in this field.</p>
+        <p className="text-3xl text-white font-semibold text-center">Tony Montana</p>
+        <p className="text-xl text-white/50 font-normal text-center mb-5">İşte birkaç sosyal medya daha</p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 w-full gap-4 items-center mt-2">
           {_projects ? (
             projects ? (
               projects.map((_, __) => (
                 <a href={_.link} target="_blank" rel="noreferrer" key={__} className="bg-neutral-800/10 p-4 hover:bg-neutral-800/20 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full">
-                  <img alt="umut" src={_.image} width="1024" className="rounded-lg" height="512" />
+                  <img alt="tony" src={_.image} width="1024" className="rounded-lg" height="512" />
                   <p className="text-xl font-semibold mt-5">{_.name}</p>
                   <p className="text-md font-normal text-white/50 h-24 overflow-small" style={{ "overflow": 'auto' }}>{_.description}</p>
                 </a>
