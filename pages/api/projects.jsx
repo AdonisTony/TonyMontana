ceab79e5433d0c7152c414e0900af883ba5bf0c3
AdonisTony <121732631+AdonisTony@users.noreply.github.com>
2023-02-23 22:05:17 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -3,16 +3,16 @@ export default async (req, res) => {
     {
       id: 1,
       image: "https://pixelbox.ru/wp-content/uploads/2021/09/anime-gif-discord-16.gif",
-      name: "Code Share",
-      description: "Discord türkiyenin en büyük discord bot komutları arşivi ve en büyük discord botu uptime, hostlayıcı websitesi. Code Share veritabanında discord botlarına özgü yüzlerce discord bot komutu bulunmaktadır ve kendi discord botunuzu yaratmanıza yardımcı olur.",
-      link: "https://codeshare.me",
+      name: "Adonis Team",
+      description: "Discord ekip sunucumuza katılarak bizlere destek olabilirsiniz.",
+      link: "https://discord.gg/w7Y9nr8HMn",
     },
     {
       id: 2,
-      image: "https://media.discordapp.net/attachments/737348411568685066/999563078079361074/YT_AVATAR_NET.jpg",
-      name: "Youtube Channel",
-      description: "I am a professional editor and intermediate software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management. I post videos about discord on my youtube channel.",
-      link: "https://youtube.com/UmutBayraktarYT",
+      image: "https://cdn.discordapp.com/attachments/1064547752727564330/1078391545423745095/steamuserimages-a.akamaihd.gif",
+      name: "İnstagram Hesabım",
+      description: "Sosyal medya hesaplarımı takip ederek bana destek çıkabilirsin.",
+      link: "https://www.instagram.com/adonistonyy",
     }
   ];
   res.status(200).json(data);
